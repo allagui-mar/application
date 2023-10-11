15b49c0b0b613a0b85a6430816527306cad2e92d
hanyn1 <hanine.azizi@esprit.tn>
2023-10-11 09:10:23 +0200

    chway design yal ghali

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,13 +4,25 @@ import './Login.css';
 import LogoGo from '../assets/LogoGo.png';
 import { BrowserRouter as Router, Route, useNavigate } from 'react-router-dom';
 export default function Login(){
-
+    const navig = useNavigate();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [passwordError, setPasswordError] = useState('');
     const [isHovered, setIsHovered] = useState(true); 
 
   const toggleHover = () => {
     setIsHovered(!isHovered);
   };
-
+  
+  const  inputContainer1= {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 30,
+    height: 40,
+    borderColor: '#2980B9',
+    borderBottomWidth: 1,
+    outline: 'none',
+  };
   const cardStyles = {
     border: '2px solid #e74c3c',
     borderRadius: '10px',
@@ -33,6 +45,30 @@ export default function Login(){
     
   };
 
+  const textStyle = {
+    fontSize: 30,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginTop: 10,
+    marginLeft: -20,
+    top: 0,
+  };
+  const input = {
+    width: '75%',
+    borderWidth: 0,
+    color: 'black',
+    paddingLeft: 10,
+    borderBottomWidth: 1.5,
+    borderBottomColor: 'red',
+    fontSize: 16,
+    paddingBottom: 10,
+    outline: 'none',
+    position: 'relative',
+    marginBottom: 50, 
+  };
+
+
+
     return(
         <div>
             <img src={LogoGo} alt="logo" className="animat-log"/>
@@ -44,11 +80,29 @@ export default function Login(){
     >
       <div className="card4-content">
         <h2>LOG<span style={{color: 'red'}}>IN</span></h2>
+        <input
+          style={input}
+          placeholder="Email ID"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+<div>
+<input
+          style={input}
+          placeholder="Password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+</div>
+
        
       </div>
       </div>
        
-      <img src={LogoGo} alt="logo" className="log"/>
+      
      
 
         </div>
